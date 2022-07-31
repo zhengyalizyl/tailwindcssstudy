cd8672ed9ab2cdf92f36a3f6c088133ecd96f716
lizheng <v-lizheng@expediagroup.com>
2022-07-31 10:53:00 +0800

    add style

---
+++ public/index.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("burger");
+const menu = document.getElementById('menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
